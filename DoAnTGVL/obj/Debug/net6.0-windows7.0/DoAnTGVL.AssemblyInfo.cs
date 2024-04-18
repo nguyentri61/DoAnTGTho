fa6b11a7163f41136f5f18fde3b3b1e7df882184
nguyentri61 <nguyenphanminhtri612004@gmail.com>
2024-04-18 12:27:56 +0700

    Fix HoaDon

--- DoAnTGVL/obj/Debug/net6.0-windows7.0/DoAnTGVL.AssemblyInfo.cs
+++ DoAnTGVL/obj/Debug/net6.0-windows7.0/DoAnTGVL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnTGVL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4dabb308e4db51659e52f4b12f83704d4f1e4a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5d8d68505ebe43cdead84b1067bf6f755dae1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnTGVL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnTGVL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
